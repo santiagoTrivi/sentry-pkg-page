8f17c8ba6d95f698ed793134b93c9c4c84a99dca
santiagoTrivi <santiagocarvajal103@gmail.com>
2025-07-07 01:42:56 -0400

    update: icosn

--- src/pages/endpoints.tsx
+++ src/pages/endpoints.tsx
@@ -21,58 +21,32 @@ export const Endpoints = () => {
 
         <div className="text-white p-4 rounded-lg overflow-x-auto mb-6">
           <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="pure-css-table">
+              <thead>
                 <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Registro
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Descripción
-                  </th>
+                  <th scope="col">Registro</th>
+                  <th scope="col">Descripción</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Registro de Usuario
-                  </th>
-                  <td className="px-6 py-4">
-                    Permite a un nuevo usuario registrarse en el sistema.
-                  </td>
+                <tr>
+                  <th scope="row">Registro de Usuario</th>
+                  <td>Permite a un nuevo usuario registrarse en el sistema.</td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Endpoint
-                  </th>
-                  <td className="px-6 py-4">POST /auth/signup</td>
+                <tr>
+                  <th scope="row">Endpoint</th>
+                  <td>POST /auth/signup</td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Descripción
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Descripción</th>
+                  <td>
                     Crea una nueva cuenta de usuario con la información
                     proporcionada.
                   </td>
                 </tr>
-                <tr className="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Petición
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Cuerpo de la Petición</th>
+                  <td>
                     Tipo: application/json Esquema:
                     <SyntaxHighlighter language="json" style={a11yLight}>
                       {`{ "email": "string", "password": "string", "username": "string" }`}
@@ -90,85 +64,51 @@ export const Endpoints = () => {
         </h4>
         <div className="text-white p-4 rounded-lg overflow-x-auto mb-6">
           <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="pure-css-table">
+              <thead>
                 <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Inicio de Sesión
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Descripción
-                  </th>
+                  <th scope="col">Inicio de Sesión</th>
+                  <th scope="col">Descripción</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Inicio de Sesión de Usuario
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Inicio de Sesión de Usuario</th>
+                  <td>
                     Permite a un usuario existente iniciar sesión y obtener un
                     token de autenticación.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Endpoint
-                  </th>
-                  <td className="px-6 py-4">POST /auth/login</td>
+                <tr>
+                  <th scope="row">Endpoint</th>
+                  <td>POST /auth/login</td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Descripción
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Descripción</th>
+                  <td>
                     Autentica a un usuario utilizando sus credenciales y emite
                     un token de acceso y un token de refresco.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Protección
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Protección</th>
+                  <td>
                     Requiere un proceso de autenticación previo para validar las
                     credenciales.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Petición
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Cuerpo de la Petición</th>
+                  <td>
                     Tipo: application/json Esquema:
                     <SyntaxHighlighter language="json" style={a11yLight}>
                       {`{ "email": "string", "password": "string" }`}
                     </SyntaxHighlighter>
                   </td>
                 </tr>
-                <tr className="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Respuesta
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Cuerpo de la Respuesta</th>
+                  <td>
                     Esquema:
                     <SyntaxHighlighter language="json" style={a11yLight}>
                       {`{ "access_token": "string", "refresh_token": "string" }`}
@@ -186,59 +126,35 @@ export const Endpoints = () => {
         </h4>
         <div className="text-white p-4 rounded-lg overflow-x-auto mb-6">
           <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="pure-css-table">
+              <thead>
                 <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Refrescar Tokens
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Descripción
-                  </th>
+                  <th scope="col">Refrescar Tokens</th>
+                  <th scope="col">Descripción</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Refrescar Tokens de Acceso
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Refrescar Tokens de Acceso</th>
+                  <td>
                     Permite obtener un nuevo token de acceso utilizando un token
                     de actualización válido.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Endpoint
-                  </th>
-                  <td className="px-6 py-4">POST /auth/refresh</td>
+                <tr>
+                  <th scope="row">Endpoint</th>
+                  <td>POST /auth/refresh</td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Descripción
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Descripción</th>
+                  <td>
                     Intercambia un token caducado por un nuevo token de acceso.
                     Mantiene el token de actualización hasta que expire.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Protección
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Protección</th>
+                  <td>
                     Requiere un bearer token{" "}
                     <code className="bg-gray-200 px-2 py-1 rounded">
                       refresh token
@@ -246,23 +162,13 @@ export const Endpoints = () => {
                     .
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Petición
-                  </th>
-                  <td className="px-6 py-4">Ninguno.</td>
+                <tr>
+                  <th scope="row">Cuerpo de la Petición</th>
+                  <td>Ninguno.</td>
                 </tr>
-                <tr className="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Respuesta
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Cuerpo de la Respuesta</th>
+                  <td>
                     Esquema:
                     <SyntaxHighlighter language="json" style={a11yLight}>
                       {`{ "access_token": "string" }`}
@@ -280,59 +186,37 @@ export const Endpoints = () => {
         </h4>
         <div className="text-white p-4 rounded-lg overflow-x-auto mb-6">
           <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <table className="pure-css-table">
+              <thead>
                 <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Obtener Información del Usuario
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Descripción
-                  </th>
+                  <th scope="col">Obtener Información del Usuario</th>
+                  <th scope="col">Descripción</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
+                <tr>
+                  <th scope="row">
                     Obtener Información del Usuario Autenticado
                   </th>
-                  <td className="px-6 py-4">
+                  <td>
                     Permite a un usuario autenticado obtener su propia
                     información de perfil.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Endpoint
-                  </th>
-                  <td className="px-6 py-4">GET /auth/me</td>
+                <tr>
+                  <th scope="row">Endpoint</th>
+                  <td>GET /auth/me</td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Descripción
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Descripción</th>
+                  <td>
                     Retorna los detalles del perfil del usuario actualmente
                     autenticado.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Protección
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Protección</th>
+                  <td>
                     Requiere un bearer token Requiere{" "}
                     <code className="bg-gray-200 px-2 py-1 rounded">
                       refresh token
@@ -340,23 +224,13 @@ export const Endpoints = () => {
                     , un token de acceso válido en el encabezado Authorization.
                   </td>
                 </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Petición
-                  </th>
-                  <td className="px-6 py-4">Ninguno.</td>
+                <tr>
+                  <th scope="row">Cuerpo de la Petición</th>
+                  <td>Ninguno.</td>
                 </tr>
-                <tr className="bg-white dark:bg-gray-800">
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Cuerpo de la Respuesta
-                  </th>
-                  <td className="px-6 py-4">
+                <tr>
+                  <th scope="row">Cuerpo de la Respuesta</th>
+                  <td>
                     Esquema:
                     <SyntaxHighlighter language="json" style={a11yLight}>
                       {`{ "id": "string", "username": "string", "email": "string", "createdAt": "timestamp", "updatedAt": "timestamp" }`}
